45bb36848e8380bbb293481a6ef10421f7f5ef9b
Christian Joseph <christianj91@gmail.com>
2021-06-06 12:47:49 -0400

    moving over application

---
+++ README.md
@@ -0,0 +1,57 @@
+# Team Profile Generator
+A command-line application built with Node.js that generates an employee roster based on user input. Add managers, engineers, and interns to your teams roster with ease.
+  
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  
+  
+  ## Table of Contents
+  1. [Installation](#Installation)
+  2. [Usage](#Usage)
+  3. [Video](#Video)
+  4. [License](#License)
+  5. [Contributing](#Contributing)
+  6. [Questions](#Questions)
+  
+  ## Installation 
+  Clone this repository by clicking on the green button that says 'Code'
+  
+  Remeber to run the command 
+  
+  > npm i
+  
+  Or
+  
+  > npm install
+  
+  to install dependencies before running the application your command line.
+  
+  ## Usage
+  To start application, use the command below.
+  
+  > node app.js 
+  
+  Answer the prompts that appear in the command line. You will be asked to add a manager, engineer, and/or intern. From there, you may enter each employee's email, Id, and role within the team. After completing these steps, this message will appear in the terminal: "A file containing your roster has been generated!"
+  If there is an error in generating this file, an error message will appear in the terminal. 
+  
+  An HTML file will then be generated based on user input and should appear in the 'output' folder. Open the HTML file in a browswer to view your teams generated roster. 
+  
+  ## Video
+  
+  <a href="https://drive.google.com/file/d/1PNjQvwtJzV1M5j6WrK2z6pAoiLzBKE1a/view">Demo Video</a>
+  
+ 
+  
+  ## License
+  This project operates under the MIT license
+  
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  
+  ## Contributing 
+  Contributions are welcome
+  
+  ## Questions
+  
+  GITHUB: https://github.com/cjose26
+  EMAIL: christianj91@gmail.com
+  LINKEDIN: https://www.linkedin.com/in/christian-joseph-4ba63535
+  
---
+++ app.js
@@ -0,0 +1,166 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+const team = [];
+
+//create new managers
+const createManager = () => {
+    inquirer.prompt([
+        {
+            name: 'name',
+            message: 'What is the name of this employee?',
+            type:'input',
+        },
+        {
+            name: 'id',
+            message: 'What is the ID of this employee?',
+            type:'input',
+
+        },  
+        {
+            name: 'email',
+            message: 'What is the Email of this employee?',
+            type:'input',
+
+        },  
+        {
+            name: 'office',
+            message: 'What is the office number of this employee?',
+            type:'input',
+
+        },
+    ]).then(answers => {
+        const newManager = new Manager(answers.name, answers.id, answers.email, answers.office)
+        team.push(newManager)
+
+        addEmployee();
+    })
+}
+
+
+//create new engineers
+const createEngineer =() => {
+    inquirer.prompt([
+        {
+            name: 'name',
+            message: 'What is the name of this employee?',
+            type:'input',
+        },
+        {
+            name: 'id',
+            message: 'What is the ID of this employee?',
+            type:'input'
+        },  
+        {
+            name: 'email',
+            message: 'What is the Email of this employee?',
+            type:'input'
+        }, 
+        {
+            name: 'github',
+            message: 'What is the GitHub username of this employee?',
+            type:'input',
+        }
+    ]).then((answers)=> {
+        const newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+        team.push(newEngineer)
+
+        addEmployee();
+    })
+}
+
+const createIntern = () => {
+    inquirer.prompt([
+        {
+            name: 'name',
+            message: 'What is the name of this employee?',
+            type:'input',
+        },
+        {
+            name: 'id',
+            message: 'What is the ID of this employee?',
+            type:'input',
+
+        },  
+        {
+            name: 'email',
+            message: 'What is the Email of this employee?',
+            type:'input',
+
+        },  
+        {
+            name: 'school',
+            message: 'What school did this intern graduate from?',
+            type:'input',
+
+        },
+    ]).then(answers => {
+        const newIntern = new Intern(answers.name, answers.id, answers.email, answers.school)
+        team.push(newIntern)
+
+        addEmployee()
+        
+    })
+    
+}
+
+
+
+const typeOfEmployee = () => {
+    inquirer.prompt([
+        {
+            name: 'employee',
+            message: 'What type of employee would you like to add?',
+            type:'list',
+            choices: [
+                {name:'Manager', value: 0},
+                {name: 'Software Engineer', value: 1},
+                {name:'Intern', value: 2},
+            ]
+        },
+    ]).then((answers) => {
+            if (answers.employee === 0) {
+                createManager();
+            }
+            else if (answers.employee === 1) {
+                createEngineer();
+            }
+            else if (answers.employee === 2) {
+                createIntern();
+            }
+        }   
+    ).catch((err) => console.error(err));
+}
+
+typeOfEmployee();
+    
+
+function addEmployee() {
+    inquirer.prompt([
+        {
+            name: 'NewMember',
+            message: 'Would you like to add another teammate?',
+            type: 'confirm',
+        }
+    ]).then((answers)=> {
+        if (answers.NewMember) {
+            typeOfEmployee();
+        } else {
+            const renderTeam = render(team) 
+            fs.writeFileSync(outputPath, renderTeam)
+
+            console.log('A file containing your roster has been generated!');
+        }
+    }).catch((err) => console.error(err));
+
+}
+
